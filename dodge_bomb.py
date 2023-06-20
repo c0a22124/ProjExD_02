0b22d343d6189b96eb7715b0b869ee5586a0feda
yuki <c0a2212499@edu.teu.ac.jp>
2023-06-20 14:44:31 +0900

    問題3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 
 def main():
@@ -11,6 +17,8 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900,400
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     bd_img = pg.Surface((20, 20))
@@ -28,9 +36,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
+        for k, mv in delta.items():
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx,vy)
         screen.blit(bd_img,bd_rct)
         
